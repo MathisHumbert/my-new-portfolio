5b7545e2e3b0e47771661a9b43134473bc1f02f0
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-09-21 09:49:35 +0200

    added icon and reformated data

---
+++ utils/data.tsx
@@ -0,0 +1,257 @@
+export const home_data = {
+  nav_data: [
+    {
+      id: 1,
+      title: 'intro',
+    },
+    {
+      id: 2,
+      title: 'about',
+    },
+    {
+      id: 3,
+      title: 'experience',
+    },
+    {
+      id: 4,
+      title: 'projects',
+    },
+    {
+      id: 5,
+      title: 'skills',
+    },
+    {
+      id: 6,
+      title: 'contact',
+    },
+  ],
+  skills_header_data: [
+    {
+      id: 1,
+      title: 'front end',
+    },
+    {
+      id: 2,
+      title: 'back end',
+    },
+    {
+      id: 3,
+      title: 'global',
+    },
+  ],
+  front_end_skills_data: [
+    {
+      id: 1,
+      title: 'Javascript',
+      icon: '/icons/typescript.svg',
+      color:
+        'invert(95%) sepia(82%) saturate(1506%) hue-rotate(338deg) brightness(101%) contrast(94%)',
+      time: '16',
+    },
+    {
+      id: 1,
+      title: 'TypeScript',
+      icon: '/icons/typescript.svg',
+      color:
+        'invert(33%) sepia(72%) saturate(757%) hue-rotate(176deg) brightness(107%) contrast(85%)',
+      time: '3',
+    },
+    {
+      id: 2,
+      title: 'React',
+      icon: '/icons/react.svg',
+      color:
+        'invert(85%) sepia(22%) saturate(5486%) hue-rotate(167deg) brightness(107%) contrast(97%)',
+      time: '12',
+    },
+    {
+      id: 3,
+      title: 'Next.js',
+      icon: '/icons/nextdotjs.svg',
+      color:
+        'invert(0%) sepia(100%) saturate(0%) hue-rotate(21deg) brightness(97%) contrast(103%)',
+      time: '4',
+    },
+    {
+      id: 4,
+      title: 'React Native',
+      icon: '/icons/react.svg',
+      color:
+        'invert(85%) sepia(22%) saturate(5486%) hue-rotate(167deg) brightness(107%) contrast(97%)',
+      time: '6',
+    },
+    {
+      id: 5,
+      title: 'Redux',
+      icon: '/icons.redux.svg',
+      color:
+        'invert(28%) sepia(75%) saturate(1715%) hue-rotate(245deg) brightness(86%) contrast(79%)',
+      time: '12',
+    },
+    {
+      id: 6,
+      title: 'HTML',
+      icon: '/icons/html5.svg',
+      color:
+        'invert(51%) sepia(99%) saturate(4340%) hue-rotate(348deg) brightness(93%) contrast(90%)',
+      time: '18',
+    },
+    {
+      id: 7,
+      title: 'CSS',
+      icon: '/icons/css3.svg',
+      color:
+        'invert(30%) sepia(100%) saturate(1060%) hue-rotate(181deg) brightness(93%) contrast(86%)',
+      time: '18',
+    },
+    {
+      id: 8,
+      title: 'Styled C.',
+      icon: '/icons/styledcomponents.svg',
+      color:
+        'invert(71%) sepia(31%) saturate(2870%) hue-rotate(297deg) brightness(90%) contrast(89%)',
+      time: '12',
+    },
+    {
+      id: 9,
+      title: 'Sass',
+      icon: '/icons/sass.svg',
+      color:
+        'invert(61%) sepia(11%) saturate(2206%) hue-rotate(280deg) brightness(82%) contrast(94%)',
+      time: '14',
+    },
+  ],
+  back_end_skills_data: [
+    {
+      id: 1,
+      title: 'NodeJS',
+      icon: '/icons/nodedotejs.svg',
+      color:
+        'invert(45%) sepia(7%) saturate(4292%) hue-rotate(71deg) brightness(107%) contrast(90%)',
+      time: '12',
+    },
+    {
+      id: 2,
+      title: 'Express',
+      icon: '/icons/express.Svg',
+      color:
+        'invert(0%) sepia(3%) saturate(7500%) hue-rotate(194deg) brightness(98%) contrast(103%)',
+      time: '12',
+    },
+    {
+      id: 3,
+      title: 'MongoDB',
+      icon: '/icons/mongodb',
+      color:
+        'invert(46%) sepia(100%) saturate(306%) hue-rotate(71deg) brightness(96%) contrast(85%)',
+      time: '12',
+    },
+    {
+      id: 4,
+      title: 'Firebase',
+      icon: 'icons/firebase.svg',
+      color:
+        'invert(90%) sepia(11%) saturate(5122%) hue-rotate(334deg) brightness(106%) contrast(103%)',
+      time: '6',
+    },
+  ],
+  gloab_skills_data: [
+    {
+      id: 1,
+      title: 'Figma',
+      icon: '/icons/figma.svg',
+      color:
+        'invert(45%) sepia(100%) saturate(3822%) hue-rotate(351deg) brightness(98%) contrast(94%)',
+      time: '12',
+    },
+
+    {
+      id: 2,
+      title: 'Git',
+      icon: '/icons/git.svg',
+      color:
+        'invert(38%) sepia(75%) saturate(2771%) hue-rotate(344deg) brightness(99%) contrast(90%)',
+      time: '14',
+    },
+    {
+      id: 3,
+      title: 'Github',
+      icon: '/icons/github.svg',
+      color:
+        'invert(5%) sepia(5%) saturate(532%) hue-rotate(314deg) brightness(92%) contrast(90%)',
+      time: '14',
+    },
+    {
+      id: 5,
+      title: 'Postman',
+      icon: '/icons/postman.svg',
+      color:
+        'invert(45%) sepia(85%) saturate(488%) hue-rotate(329deg) brightness(101%) contrast(103%)',
+      time: '12',
+    },
+  ],
+  projects_data: [
+    {
+      id: 1,
+      src: '/assets/project-1.jpg',
+      title: 'MERN Project',
+      subtitle: 'Invoice App',
+      category: 'Full Stack Project - Frontend Mentor',
+      tech: 'TypeScript / JavaScript / React / Redux / Styled Components / NodeJS / Express / MongoDB',
+      code: 'https://github.com/MathisHumbert/invoice-app',
+      website: 'https://invoice-app-ts-mern.herokuapp.com/',
+    },
+    {
+      id: 2,
+      src: '/assets/project-2.jpg',
+      title: 'React Landing Page',
+      subtitle: 'Skillex',
+      category: 'Front End Project - cloned website from Dribbble',
+      tech: 'JavaScript / React / Styled Components / GSAP',
+      code: 'https://github.com/MathisHumbert/react-clone-skillex-website',
+      website: 'https://cloned-skillex-webiste.netlify.app/',
+    },
+    {
+      id: 3,
+      src: '/assets/project-3.jpg',
+      title: 'Full Stack JavaScript',
+      subtitle: 'Product Feedback',
+      category: 'Full Stack Project - Frontend Mentor',
+      tech: 'JavaScirpt / Sass / HTML / NodeJS / Express / MongoDB',
+      code: 'https://github.com/MathisHumbert/product-feedback-full-stack-app',
+      website: 'https://product-feedback-fullstack-app.herokuapp.com/',
+    },
+  ],
+  footer_data: [
+    {
+      id: 1,
+      logo: '/icons/twitter.svg',
+      url: 'https://twitter.com/Mathis1Humbert',
+    },
+    {
+      id: 2,
+      logo: '/icons/github.svg',
+      url: 'https://github.com/MathisHumbert',
+    },
+    {
+      id: 3,
+      logo: '/icons/linkedin.svg',
+      url: 'https://www.linkedin.com/in/mathis-humbert-1612ba1b5/',
+    },
+    {
+      id: 5,
+      logo: '/icons/freecodecamp.svg',
+      url: 'https://www.freecodecamp.org/mathisse',
+    },
+    {
+      id: 4,
+      logo: '/icons/frontendmentor.svg',
+      url: 'https://www.frontendmentor.io/profile/MathisHumbert',
+    },
+    {
+      id: 6,
+      logo: '/icons/codewars.svg',
+      url: 'https://www.codewars.com/users/MathisHumbert',
+    },
+  ],
+};
